b7657c1524fd6164204fb043a147babaaafe7327
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-31 23:51:56 +0100

    added arrayMethod all five task !

--- js/arrayMethods.js
+++ js/arrayMethods.js
@@ -19,4 +19,27 @@ if (toDoList.includes("Attend Meeting")){
     toDoList.splice(indexOfMetteing,1)
 }
 
-console.log(toDoList)
+// console.log(toDoList)
+
+// Task3: You want to take break and do something relaxing. Extract the task related to relaxation 
+// (index 2 to index , inclusive) into a new array called relaxationTask 
+
+const relaxationTask = toDoList.slice(2,5)
+
+//console.log(relaxationTask)
+
+// Task4: You want to check if "Buy Groceries" is still on your list. If it is, mark it as completed by removing it
+
+if (toDoList.includes("Buy Groceries")){
+    const indexOfGroceries = toDoList.indexOf("Buy Groceries");
+    toDoList.splice(indexOfGroceries,1);
+}
+// console.log(toDoList);
+
+// Task:5 You receeive a new task "Call a Friend" but you are not sure if it's already on your list. check if it 
+// is there . if not add it! 
+
+if (!toDoList.includes("Call a Friend")){
+    toDoList.push("Call a Friend");
+}
+console.log(toDoList);
