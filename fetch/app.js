4225a6f900843c6e5b01247b130935458f28d8b1
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-02 02:43:02 +0100

    added fetch request!!

---
+++ fetch/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let products = document.querySelector('.products');
+    async function fetchProduct(url) {
+        let data = await fetch(url);
+        let response = await data.json();
+
+        for (let i = 0; i <response.length; i++){
+            let description = response[i].description;
+            products.innerHTML = `
+        <div class="product">
+            <img src="${response[i].image}" alt="" class="product-img">
+            <h2 class="product-title">${response[i].title}</h2>
+            <h4 class="product-catagory">${response[i].genre}</h4>
+            <p class="product-description">${description}</p>
+            <div class="product-price-container">
+                <h3 class="product-price">${response[i].price}</h3>
+                <a href="#!"  data-productId="${response[i].id}" class="add-to-cart"></a>
+            </div>
+        </div>
+        `;
+        }
+        
+        
+    };
+    fetchProduct('https://api.noroff.dev/api/v1/square-eyes');
+});
