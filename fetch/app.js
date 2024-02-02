0a0fa05d4d68984d5dd9de557c4be1aa4a8fedab
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-02 03:02:00 +0100

    added basic styles!!

--- fetch/app.js
+++ fetch/app.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function(){
             <img src="${response[i].image}" alt="" class="product-img">
             <h2 class="product-title">${response[i].title}</h2>
             <h4 class="product-catagory">${response[i].genre}</h4>
-            <p class="product-description">${description.length > 20 ? description.substring(0,80).concat('...more') : description}</p>
+            <p class="product-description">${description.length > 80 ? description.substring(0,80).concat('...more') : description}</p>
             <div class="product-price-container">
                 <h3 class="product-price">${response[i].price}</h3>
                 <a href="#!"  data-productId="${response[i].id}" class="add-to-cart"></a>
