d1ac9bba9e0359260c47ccebfb4e20b45bf19ca3
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-02 02:48:38 +0100

    added all data from API! !

--- fetch/app.js
+++ fetch/app.js
@@ -6,12 +6,12 @@ document.addEventListener('DOMContentLoaded', function(){
 
         for (let i = 0; i <response.length; i++){
             let description = response[i].description;
-            products.innerHTML = `
+            products.innerHTML += `
         <div class="product">
             <img src="${response[i].image}" alt="" class="product-img">
             <h2 class="product-title">${response[i].title}</h2>
             <h4 class="product-catagory">${response[i].genre}</h4>
-            <p class="product-description">${description}</p>
+            <p class="product-description">${description.length > 20 ? description.substring(0,80).concat('...more') : description}</p>
             <div class="product-price-container">
                 <h3 class="product-price">${response[i].price}</h3>
                 <a href="#!"  data-productId="${response[i].id}" class="add-to-cart"></a>
