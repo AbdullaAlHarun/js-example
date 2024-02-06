4eac305613ce1ced25495895f2f5d65546a2948e
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-06 15:38:06 +0100

    added fetch with catagory button!

--- fetch/app.js
+++ fetch/app.js
@@ -9,13 +9,15 @@ document.addEventListener('DOMContentLoaded', function(){
             products.innerHTML += `
         <div class="product">
             <img src="${response[i].image}" alt="" class="product-img">
-            <h2 class="product-title">${response[i].title}</h2>
-            <h4 class="product-catagory">${response[i].genre}</h4>
-            <p class="product-description">${description.length > 80 ? description.substring(0,80).concat('...more') : description}</p>
-            <div class="product-price-container">
-                <h3 class="product-price">${response[i].price}</h3>
-                <a href="#!"  data-productId="${response[i].id}" class="add-to-cart"></a>
-            </div>
+           <div class="product-content">
+                <h2 class="product-title">${response[i].title}</h2>
+                <h4 class="product-catagory">${response[i].genre}</h4>
+                <p class="product-description">${description.length > 80 ? description.substring(0,80).concat('...more') : description}</p>
+                <div class="product-price-container">
+                    <h3 class="product-price">$${response[i].price}</h3>
+                    <a href="#!"  data-productId="${response[i].id}" class="add-to-cart">Add To Cart</a>
+                </div>
+           </div>
         </div>
         `;
         }
