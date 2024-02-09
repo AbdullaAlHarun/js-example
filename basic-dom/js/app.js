a099d1233af67501d3353aebdfb8da0628aeca7c
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-09 17:44:08 +0100

    Traversing Dom - GetElementsByTagName!!

---
+++ basic-dom/js/app.js
@@ -0,0 +1,14 @@
+const allHeading = document.getElementsByTagName('h1');
+// console.log(bodyElement.);
+
+for (const h1 of allHeading) {
+
+    console.log(h1.innerText);
+    
+}
+
+const liCollection = document.getElementsByTagName('li');
+// console.log(liCollection);
+for (const li of liCollection) {
+    console.log(li.innerText);
+}
