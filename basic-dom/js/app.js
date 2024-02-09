c2f74dccc058e828a25747d7d3c30d5e87e47938
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-09 18:00:09 +0100

    Traversing Dom - GetElementByClass VS GetElementById!!

--- basic-dom/js/app.js
+++ basic-dom/js/app.js
@@ -3,12 +3,17 @@ const allHeading = document.getElementsByTagName('h1');
 
 for (const h1 of allHeading) {
 
-    console.log(h1.innerText);
+    // console.log(h1.innerText);
     
 }
 
 const liCollection = document.getElementsByTagName('li');
 // console.log(liCollection);
 for (const li of liCollection) {
-    console.log(li.innerText);
+    // console.log(li.innerText);
 }
+
+const fruitTitle =  document.getElementById('fruits-title');
+ fruitTitle.innerText = 'Fruits change by JS'
+ console.log(fruitTitle)
+
