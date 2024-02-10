dcb24ea6878d44d93a075d43fab37fc297d4311d
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-10 16:56:38 +0100

    addEventListener example!

--- event/js/app.js
+++ event/js/app.js
@@ -1,3 +1,7 @@
+// option 1 
+//    <button onclick="console.log(4)">click me</button>
+//  <!-- <button onclick="document.body.style.backgroundColor='red'" >Hey Hey</button> -->
+
 //option: 2 
 function makeRed(){
     document.body.style.backgroundColor = 'red';
@@ -14,3 +18,23 @@ const purpleButton = document.getElementById('make-purple');
 purpleButton.onclick = function makePurple(){
 document.body.style.backgroundColor = 'purple';
 }
+
+//option: 4 
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink)
+
+function makePink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+//option 4 another 
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'green';
+})
+
+//option 5 final 
+
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+})
