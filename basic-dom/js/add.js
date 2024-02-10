9cb2b0a557c4631c8869efc1bdb05da967df4001
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-10 15:03:07 +0100

    adde one unorderd list on dom element!

--- basic-dom/js/add.js
+++ basic-dom/js/add.js
@@ -1,5 +1,29 @@
-const placeList = document.getElementById('places-list');
-const li = document.createElement('li');
-li.innerText = 'Norway';
+// const placeList = document.getElementById('places-list');
+// const li = document.createElement('li');
+// li.innerText = 'Norway';
+// placeList.appendChild(li);
 
-placeList.appendChild(li);
+//where to add 
+const mainContainer = document.getElementById('main-container');
+
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'My food list';
+section.appendChild(h1);
+
+const ul = document.createElement('ul');
+const li1 = document.createElement('li');
+li1.innerText = 'Biryani';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li');
+li2.innerText = 'kala Vuna';
+ul.appendChild(li2);
+
+const li3 = document.createElement('li');
+li3.innerText = 'haser mangsho';
+ul.appendChild(li3);
+
+section.appendChild(ul);
+
+mainContainer.appendChild(section);
