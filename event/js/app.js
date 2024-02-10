cde3383fb91f33f18186085ae3d9e593669bc1c7
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-10 16:56:24 +0100

    some example of on click events!

--- event/js/app.js
+++ event/js/app.js
@@ -1,3 +1,16 @@
+//option: 2 
 function makeRed(){
     document.body.style.backgroundColor = 'red';
+}
+
+//option: 3
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+function makeBlue(){
+    document.body.style.backgroundColor = 'blue';
+}
+
+const purpleButton = document.getElementById('make-purple');
+purpleButton.onclick = function makePurple(){
+document.body.style.backgroundColor = 'purple';
 }
