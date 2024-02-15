3bac3ee6ba29e5f925e745c3e919e63632ef5098
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-16 00:16:59 +0100

    fixed bugs!

--- fetch/app.js
+++ fetch/app.js
@@ -1,29 +1,35 @@
-document.addEventListener('DOMContentLoaded', function(){
+const mainDocuments = document.addEventListener('DOMContentLoaded', function(){
+    fetchProduct('https://api.noroff.dev/api/v1/square-eyes');
     let products = document.querySelector('.products');
-    
+    let genreLinks = document.querySelectorAll('.catagory ul li a');
+
     async function fetchProduct(url) {
         let data = await fetch(url);
         let response = await data.json();
 
-        for (let i = 0; i <response.length; i++){
-            let description = response[i].description;
-            products.innerHTML += `
-        <div class="product">
-            <img src="${response[i].image}" alt="" class="product-img">
-           <div class="product-content">
-                <h2 class="product-title">${response[i].title}</h2>
-                <h4 class="product-catagory">${response[i].genre}</h4>
-                <p class="product-description">${description.length > 80 ? description.substring(0,80).concat('...more') : description}</p>
-                <div class="product-price-container">
-                    <h3 class="product-price">$${response[i].price}</h3>
-                    <a href="#!"  data-productId="${response[i].id}" class="add-to-cart">Add To Cart</a>
-                </div>
-           </div>
-        </div>
-        `;
-        }
+        displayProducts(response);
         
-        
+      
     };
-    fetchProduct('https://api.noroff.dev/api/v1/square-eyes');
+
+    function displayProducts(productsArray) {
+        products.innerHTML = '';
+        for (let i = 0; i < productsArray.length; i++) {
+            let description = productsArray[i].description;
+            products.innerHTML += `
+                <div class="product">
+                    <img src="${productsArray[i].image}" alt="" class="product-img">
+                    <div class="product-content">
+                        <h2 class="product-title">${productsArray[i].title}</h2>
+                        <h4 class="product-catagory">${productsArray[i].genre}</h4>
+                        <p class="product-description">${description.length > 80 ? description.substring(0,80).concat('...more') : description}</p>
+                        <div class="product-price-container">
+                            <h3 class="product-price">$${productsArray[i].price}</h3>
+                            <a href="#!"  data-productId="${productsArray[i].id}" class="add-to-cart">Add To Cart</a>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+    }
 });
